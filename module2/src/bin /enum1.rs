d0c70a452cc77ae56a31d5a1c1e0c09c434ff122
Ritvik <91389059+raghav-rama@users.noreply.github.com>
2022-11-11 18:39:18 +0000

    solve enum1.rs

--- module2/src/bin /enum1.rs
+++ module2/src/bin /enum1.rs
@@ -17,10 +17,31 @@
 
 
 fn main() {
-
-
-
-}
+    let mut colors: Vec<Colors> = Vec::new();
+    colors.push(Colors::Red);
+    colors.push(Colors::Green);
+    colors.push(Colors::Blue);
+    for color in colors {
+    colors.push(Colors::Yellow);
+      print_color(color);
+    }
+  }
+  
+  fn print_color(color: Colors) {
+    match color {
+      Colors::Red => println!("Red"),
+      Colors::Green => println!("Green"),
+      Colors::Blue => println!("Blue"),
+      Colors::Yellow => println!("Yellow"),
+    }
+  }
+  
+  enum Colors {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+  }
  
 
 
