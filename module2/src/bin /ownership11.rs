e436aac24293a1a1b1bc4a7e0ecc175298f9ca46
raghav-rama <hackerer528@gmail.com>
2022-11-24 22:54:42 +0000

    solve ownership11.rs

--- module2/src/bin /ownership11.rs
+++ module2/src/bin /ownership11.rs
@@ -3,7 +3,7 @@ fn main() {
     let t = (String::from("hello"), String::from("world"));
  
      // Fill the blanks
-     let (__, __) = __;
+     let (s1, s2) = &t;
  
      println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
  }
