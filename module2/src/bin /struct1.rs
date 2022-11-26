ef3b071d3ba251280cda00250a40f790d9385f1c
raghav-rama <hackerer528@gmail.com>
2022-11-26 22:49:35 +0000

    solve struct1.rs

--- module2/src/bin /struct1.rs
+++ module2/src/bin /struct1.rs
@@ -1,91 +1,47 @@
-// Topic: Getting Used to with struct
+// Topic: Organizing similar data using structs
 //
-//Requirements:
-/*
- 1-Define  struct Shape having  square:Square and rectangle:Rectangle 
- 2-Square has one field as side:i32
- 3-Rectangle has two length:i32 and width:i32
- 4-Create a function which returns a new Rectangle
- 5-Create a function which returns a new Square
- 6-Create a function which returns a new Shape 
- 7-Create a function which takes Shape and prints the dimension of all shapes 
-*/
+// Requirements:
+// * Print the flavor of a drink and it's fluid ounces
 
-use std::io;
+// Notes:
+// * Use an enum to create different flavors of drinks
+// * Use a struct to store drink flavor and fluid ounce information
+// * Use a function to print out the drink flavor and ounces
+// * Use a match expression to print the drink flavor
 
-struct Shape {
-    square: Square,
-    rectangle: Rectangle,
+
+fn  main() {
+    let mut drinks = Vec::new();
+    let mojito = Drinks {
+    	flavour: Flavours::Sour,
+        ounces: 32,
+    };
+    let mocha = Drinks {
+        flavour: Flavours::Sweet,
+        ounces: 64,
+    };
+    drinks.push(mojito);
+    drinks.push(mocha);
+    for drink in drinks.iter() {
+        match drink.flavour {
+            Flavours::Sweet => println!("Sweet"),
+            Flavours::Sour => println!("Sour"),
+            _ => println!("{:?}, new flavour invented by you :')", drink.flavour),
+        }
+    }
+
+
 }
 
-struct Square {
-    side: i32,
+#[derive(Debug)]
+struct Drinks {
+    flavour: Flavours,
+    ounces: i32,
 }
 
-struct Rectangle {
-    length: i32,
-    width: i32,
-}
 
-fn sq(side: i32) -> Square {
-    Square { side }
-}
-
-fn rect(length: i32, width: i32) -> Rectangle {
-    Rectangle { length, width }
-}
-
-fn shape(square: Square, rectangle: Rectangle) -> Shape {
-    Shape { square, rectangle }
-}
-
-fn print(shape: Shape) {
-    println!("Side of Square: {}", shape.square.side);
-    println!("Length of Rectangle: {}", shape.rectangle.length);
-    println!("Widht of Rectangle: {}", shape.rectangle.width);
-}
-
-fn main() {
-    let side;
-    let length;
-    let width;
-
-    let mut buf = String::new();
-
-    println!("Enter side of square:");
-    io::stdin()
-    .read_line(&mut buf)
-    .expect("Problem reading stdin");
-
-    side = buf
-    .trim()
-    .parse::<i32>()
-    .unwrap();
-
-    println!("Enter length of rectangle: ");
-    buf = String::new();
-    io::stdin()
-    .read_line(&mut buf)
-    .expect("Problem reading stdin");
-
-    length = buf
-    .trim()
-    .parse::<i32>()
-    .unwrap();
-    
-    println!("Enter width of rectangle: ");
-    buf = String::new();
-    io::stdin()
-    .read_line(&mut buf)
-    .expect("Problem reading stdin");
-    
-    width = buf
-    .trim()
-    .parse::<i32>()
-    .unwrap();
-
-    let sq = sq(side);
-    let rect =  rect(length, width);
-    let shape = shape(sq, rect);
-    print(shape);
+#[derive(Debug)]
+enum Flavours {
+    Sweet,
+    Sour,
 }
