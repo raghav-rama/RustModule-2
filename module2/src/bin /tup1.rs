37d65051ac8fcf6e0e8e42fb8dbdad358e04f0e7
raghav-rama <hackerer528@gmail.com>
2022-11-27 00:06:07 +0000

    solve tup1.rs

--- module2/src/bin /tup1.rs
+++ module2/src/bin /tup1.rs
@@ -9,10 +9,45 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
+use std::io;
 
+fn main() {
+    let (x, y) = my_tup();
+    if y > 5 {
+        println!("y coordinate is greater than 5")
+    } else if y == 5 {
+        println!("y coordinate is equal to 5")
+    } else {
+        println!("y coordinate is smaller than 5")
+    }
+}
 
+fn my_tup() -> (i32, i32) {
+    let mut my_tup = (0,0);
+    let mut buf = String::new();
 
+    println!("Enter x-coordinate: ");
+    io::stdin()
+    .read_line(&mut buf)
+    .expect("Problem reading stdin");
 
+    let x = buf
+    .trim()
+    .parse::<i32>()
+    .unwrap();
 
+    println!("Enter y-coordinate");
+    buf = String::new();
+    io::stdin()
+    .read_line(&mut buf)
+    .expect("Problem reading stdin");
 
-fn main() {}
+    let y = buf
+    .trim()
+    .parse::<i32>()
+    .unwrap();
+
+    my_tup = (x, y);
+
+    return my_tup;
+}
